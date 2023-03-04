64307eb258f0dfa36a734e6ade9b496bf6deb928
orineoguri <gomgom9kr@outlook.kr>
2023-03-04 12:25:55 +0900

    카드팩 클래스 정리

--- Orineoguri.Loa.CardUnpack/CardPacks.cs
+++ Orineoguri.Loa.CardUnpack/CardPacks.cs
@@ -18,43 +18,39 @@ namespace Orineoguri.Loa.CardUnpack
 
     public class CardPacks
     {
-        private Dictionary<int, int> _rangedCardPacks1; //범위카드팩 1
-        private Dictionary<int, int> _rangedCardPacks2; //범위카드팩 2(심연팩 포함)
+        private Dictionary<int, int>[] _rangedCardPack = new Dictionary<int, int>[2];
         private Dictionary<int, int> _rawCardPacks; //개별 카드팩
         private Random _random; //난수발생기
 
-        private readonly int ABYSS_START = 1; //심연팩 1번~19번(총 19종)
-        private readonly int ABYSS_END = 19;
-        private readonly int RELIC_START = 6; //전설팩 6번~24번(총 19종)
-        private readonly int RELIC_END = 24;
-        private readonly int HEROIC_START = 25; //영웅팩 22번~99번(총 75종)
-        private readonly int HEROIC_END = 99;
-        private readonly int RARE_START = 100; //희귀팩 100번~200번(총 101종)
-        private readonly int RARE_END = 200; 
-        private readonly int HIGH_START = 201; //고급팩 201번~257번(총 57종)
-        private readonly int HIGH_END = 257;
-        private readonly int COMMON_START = 258; //일반팩 258번~285번(총 28종)
-        private readonly int COMMON_END = 285;
+        private const int ABYSS_START = 1; //심연팩 1번~19번(총 19종)
+        private const int ABYSS_END = 19;
+        private const int RELIC_START = 6; //전설팩 6번~24번(총 19종)
+        private const int RELIC_END = 24;
+        private const int HEROIC_START = 25; //영웅팩 22번~99번(총 75종)
+        private const int HEROIC_END = 99;
+        private const int RARE_START = 100; //희귀팩 100번~200번(총 101종)
+        private const int RARE_END = 200; 
+        private const int HIGH_START = 201; //고급팩 201번~257번(총 57종)
+        private const int HIGH_END = 257;
+        private const int COMMON_START = 258; //일반팩 258번~285번(총 28종)
+        private const int COMMON_END = 285;
 
         public CardPacks(int abyss, int relic, int heroic, int rare, int high, int common, //심연팩, 전설팩, 영웅팩, 희귀팩, 고급팩, 일반팩
             int relic_heroic, int relic_rare, int relic_high, int entire, //전영팩1, 전희팩1, 전고팩1, 전체팩1
             int relic_heroic2, int relic_rare2, int relic_high2, int entire2) //전영팩2, 전희팩2, 전고팩2, 전체팩2
         {
-            this._rangedCardPacks1 = new Dictionary<int, int>();
-            this._rangedCardPacks2 = new Dictionary<int, int>();
-            this._rawCardPacks = new Dictionary<int, int>();
             this._random = new Random();
 
             //범위 카드팩들 초기화
-            _rangedCardPacks1[(int)CardRank.Heroic] = relic_heroic;
-            _rangedCardPacks1[(int)CardRank.Rare] = relic_rare;
-            _rangedCardPacks1[(int)CardRank.High] = relic_high;
-            _rangedCardPacks1[(int)CardRank.Common] = entire;
+            _rangedCardPack[0][(int)CardRank.Heroic] = relic_heroic;
+            _rangedCardPack[0][(int)CardRank.Rare] = relic_rare;
+            _rangedCardPack[0][(int)CardRank.High] = relic_high;
+            _rangedCardPack[0][(int)CardRank.Common] = entire;
 
-            _rangedCardPacks2[(int)CardRank.Heroic] = relic_heroic2;
-            _rangedCardPacks2[(int)CardRank.Rare] = relic_rare2;
-            _rangedCardPacks2[(int)CardRank.High] = relic_high2;
-            _rangedCardPacks2[(int)CardRank.Common] = entire2;
+            _rangedCardPack[1][(int)CardRank.Heroic] = relic_heroic2;
+            _rangedCardPack[1][(int)CardRank.Rare] = relic_rare2;
+            _rangedCardPack[1][(int)CardRank.High] = relic_high2;
+            _rangedCardPack[1][(int)CardRank.Common] = entire2;
 
             //개별 카드팩들 초기화
             _rawCardPacks[(int)CardRank.Abyss] = abyss;
@@ -68,73 +64,63 @@ namespace Orineoguri.Loa.CardUnpack
 
         private void UnpackRangedCardPacks()
         {
-            //전영팩 40:160 비율
-            for (int i = 0; i < _rangedCardPacks1[(int)CardRank.Heroic]; i++)
+            for(int index = 0; index < _rangedCardPack.Length; index++)
             {
-                int randomNumber = _random.Next(0, 199);
-                if(randomNumber < 40) { _rawCardPacks[(int)CardRank.Relic] += 1; }
-                else { _rawCardPacks[(int)CardRank.Heroic] += 1; }
-            }
-            for (int i = 0; i < _rangedCardPacks2[(int)CardRank.Heroic]; i++)
-            {
-                int randomNumber = _random.Next(0, 199);
-                if (randomNumber < 40) { _rawCardPacks[(int)CardRank.Abyss] += 1; } //전설팩 무조건 심연으로
-                else { _rawCardPacks[(int)CardRank.Heroic] += 1; }
-            }
+                //전영팩 40:160 비율
+                for (int i = 0; i < _rangedCardPack[index][(int)CardRank.Heroic]; i++)
+                {
+                    int randomNumber = _random.Next(0, 199);
+                    if (randomNumber < 40) 
+                    {
+                        if (index == 0) { _rawCardPacks[(int)CardRank.Relic] += 1; }
+                        else { _rawCardPacks[(int)CardRank.Abyss] += 1; }
+                    }
+                    else { _rawCardPacks[(int)CardRank.Heroic] += 1; }
+                }
 
-            //전희팩 8:32:160 비율
-            for (int i = 0; i < _rangedCardPacks1[(int)CardRank.Rare]; i++)
-            {
-                int randomNumber = _random.Next(0, 199);
-                if (randomNumber < 8) { _rawCardPacks[(int)CardRank.Relic] += 1; }
-                else if (randomNumber < 32) { _rawCardPacks[(int)CardRank.Heroic] += 1; }
-                else { _rawCardPacks[(int)CardRank.Rare] += 1; }
-            }
-            for (int i = 0; i < _rangedCardPacks2[(int)CardRank.Rare]; i++)
-            {
-                int randomNumber = _random.Next(0, 199);
-                if (randomNumber < 8) { _rawCardPacks[(int)CardRank.Abyss] += 1; } //전설팩 무조건 심연으로
-                else if (randomNumber < 32) { _rawCardPacks[(int)CardRank.Heroic] += 1; }
-                else { _rawCardPacks[(int)CardRank.Rare] += 1; }
-            }
+                //전희팩 8:32:160 비율
+                for (int i = 0; i < _rangedCardPack[index][(int)CardRank.Rare]; i++)
+                {
+                    int randomNumber = _random.Next(0, 199);
+                    if (randomNumber < 8) 
+                    {
+                        if (index == 0) { _rawCardPacks[(int)CardRank.Relic] += 1; }
+                        else { _rawCardPacks[(int)CardRank.Abyss] += 1; }
+                    }
+                    else if (randomNumber < 32) { _rawCardPacks[(int)CardRank.Heroic] += 1; }
+                    else { _rawCardPacks[(int)CardRank.Rare] += 1; }
+                }
 
-            //전고팩 4:28:94:74 비율
-            for (int i = 0; i < _rangedCardPacks1[(int)CardRank.High]; i++)
-            {
-                int randomNumber = _random.Next(0, 199);
-                if (randomNumber < 4) { _rawCardPacks[(int)CardRank.Relic] += 1; }
-                else if (randomNumber < 28) { _rawCardPacks[(int)CardRank.Heroic] += 1; }
-                else if (randomNumber < 94) { _rawCardPacks[(int)CardRank.Rare] += 1; }
-                else { _rawCardPacks[(int)CardRank.High] += 1; }
-            }
-            for (int i = 0; i < _rangedCardPacks2[(int)CardRank.High]; i++)
-            {
-                int randomNumber = _random.Next(0, 199);
-                if (randomNumber < 4) { _rawCardPacks[(int)CardRank.Abyss] += 1; } //전설팩 무조건 심연으로
-                else if (randomNumber < 28) { _rawCardPacks[(int)CardRank.Heroic] += 1; }
-                else if (randomNumber < 94) { _rawCardPacks[(int)CardRank.Rare] += 1; }
-                else { _rawCardPacks[(int)CardRank.High] += 1; }
-            }
+                //전고팩 4:28:94:74 비율
+                for (int i = 0; i < _rangedCardPack[index][(int)CardRank.High]; i++)
+                {
+                    int randomNumber = _random.Next(0, 199);
+                    if (randomNumber < 4) 
+                    {
+                        if (index == 0) { _rawCardPacks[(int)CardRank.Relic] += 1; }
+                        else { _rawCardPacks[(int)CardRank.Abyss] += 1; }
+                    }
+                    else if (randomNumber < 28) { _rawCardPacks[(int)CardRank.Heroic] += 1; }
+                    else if (randomNumber < 94) { _rawCardPacks[(int)CardRank.Rare] += 1; }
+                    else { _rawCardPacks[(int)CardRank.High] += 1; }
+                }
 
-            //전체팩 1:19:80:60:40 비율
-            for (int i = 0; i < _rangedCardPacks1[(int)CardRank.Common]; i++)
-            {
-                int randomNumber = _random.Next(0, 199);
-                if (randomNumber < 1) { _rawCardPacks[(int)CardRank.Relic] += 1; }
-                else if (randomNumber < 19) { _rawCardPacks[(int)CardRank.Heroic] += 1; }
-                else if (randomNumber < 80) { _rawCardPacks[(int)CardRank.Rare] += 1; }
-                else if (randomNumber < 60) { _rawCardPacks[(int)CardRank.High] += 1; }
-                else { _rawCardPacks[(int)CardRank.Common] += 1; }
+                //전체팩 1:19:80:60:40 비율
+                for (int i = 0; i < _rangedCardPack[index][(int)CardRank.Common]; i++)
+                {
+                    int randomNumber = _random.Next(0, 199);
+                    if (randomNumber < 1) 
+                    {
+                        if (index == 0) { _rawCardPacks[(int)CardRank.Relic] += 1; }
+                        else { _rawCardPacks[(int)CardRank.Abyss] += 1; }
+                    }
+                    else if (randomNumber < 19) { _rawCardPacks[(int)CardRank.Heroic] += 1; }
+                    else if (randomNumber < 80) { _rawCardPacks[(int)CardRank.Rare] += 1; }
+                    else if (randomNumber < 60) { _rawCardPacks[(int)CardRank.High] += 1; }
+                    else { _rawCardPacks[(int)CardRank.Common] += 1; }
+                }
             }
-            for (int i = 0; i < _rangedCardPacks2[(int)CardRank.Common]; i++)
-            {
-                int randomNumber = _random.Next(0, 199);
-                if (randomNumber < 1) { _rawCardPacks[(int)CardRank.Abyss] += 1; } //전설팩 무조건 심연으로
-                else if (randomNumber < 19) { _rawCardPacks[(int)CardRank.Heroic] += 1; }
-                else if (randomNumber < 80) { _rawCardPacks[(int)CardRank.Rare] += 1; }
-                else if (randomNumber < 60) { _rawCardPacks[(int)CardRank.High] += 1; }
-                else { _rawCardPacks[(int)CardRank.Common] += 1; }
-            }
+            
         }
 
         public Dictionary<int, int> UnpackRawCardPacks()
